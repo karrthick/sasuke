53f73da5aae46c1c07cab8c7c54a5ca1e9ad14f0
karrthick <manokarthick3110@gmail.com>
2022-06-21 14:02:26 +0530

    js are cleared

--- server.js
+++ server.js
@@ -4,7 +4,3 @@ const res = require('express/lib/response');
 const path = require('path');
 const app=express();
 app.use(express.static('./dist/res'));
-app.get('/*',(req,res)=>
-    res.sendFile('index.html',{root:'dist/res'}),
-);
-app.listen(process.env.port||8080);

6cbba9ceb8a7c1976d137d9bbc5a91188e820ab0
karrthick <manokarthick3110@gmail.com>
2022-06-21 14:33:09 +0530

    port changed

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express=require('express');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
+
 const path = require('path');
 const app=express();
-app.use(express.static('./dist/res'));
+app.use(express.static('./dist/RES'));
+app.get('/*',(req,res)=>
+    res.sendFile('index.html',{root:'dist/RES'}),
+);
+app.listen(process.env.PORT||8080);
